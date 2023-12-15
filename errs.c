6d133214a58b4318024b312415b678a33e6cfdc2
ahmed <ahmedelabide@gmail.com>
2023-12-15 09:59:19 -0800

    project ended

---
+++ errs.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ *_eputs - displays an input str
+ * @str: the str to be displayed
+ *
+ * Return: nada
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - displays the char c to stderr
+ * @c: The char to display
+ *
+ * Return: if success => 1.else => -1 and errno appropriate
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - displays the char c to input fd
+ * @c: The char to display
+ * @fd: file descriptor to print to
+ *
+ * Return: if success => 1.else => -1 and errno appropriate
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - displays given str
+ * @str: displayed str
+ * @fd: the file desc to print to
+ *
+ * Return: numb of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
---
+++ porti.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * interactive -  if shell is interactive mode =>true
+ * @info: struct addr
+ *
+ * Return:if interactive mode =>1 | otherwise =>0
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - determines if char is a delimeter
+ * @c: the char to determine
+ * @delim: the delimeter str
+ * Return: if true => 1, else => 0
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - checks for alphabetic char
+ *@c: The char to input
+ *Return: if c is alphabetic =>1 else =>0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - converts a str to an int
+ *@s: the str to be converted
+ *Return: if no numbs in str=>0, else =>converted numb
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
